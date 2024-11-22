4a1913e7810222777de0692c35e484208f234113
Janani M <141415335+Janani18022004@users.noreply.github.com>
2024-11-22 20:13:27 +0530

    Add files via upload

---
+++ 3diabetesbd.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 2.11.6
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 30, 2023 at 04:05 AM
+-- Server version: 5.0.51
+-- PHP Version: 5.2.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `2multidiseasebd`
+--
+
+-- --------------------------------------------------------
+
+
+--
+-- Table structure for table `regtb`
+--
+
+CREATE TABLE `regtb` (
+  `id` bigint(10) NOT NULL auto_increment,
+  `Name` varchar(250) NOT NULL,
+  `Email` varchar(250) NOT NULL,
+  `Mobile` varchar(250) NOT NULL,
+  `Address` varchar(250) NOT NULL,
+  `UserName` varchar(250) NOT NULL,
+  `Password` varchar(250) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `regtb`
+--
+
+INSERT INTO `regtb` (`id`, `Name`, `Email`, `Mobile`, `Address`, `UserName`, `Password`) VALUES
+(1, 'sangeeth Kumar', 'sangeeth5535@gmail.com', '9486365535', 'No 16, Samnath Plaza, Madurai Main Road, Melapudhur', 'san', 'san');
